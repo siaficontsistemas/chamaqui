908c6882af6375b5a50e3cf2728747e108cea485
kauanrubem <kauanrubem@gmail.com>
2026-06-30 20:05:22 -0300

    add changes

--- backend/helpdesk/src/main/resources/db/migration/V53__sanitize_whatsapp_transport_ids_on_users.sql
+++ backend/helpdesk/src/main/resources/db/migration/V53__sanitize_whatsapp_transport_ids_on_users.sql
@@ -25,26 +25,33 @@ begin
     select schema_name
     from companies
   loop
-    execute format($sql$
-      update %I.users
-      set whatsapp_transport_id = null
-      where whatsapp_transport_id is not null
-        and (
-          lower(whatsapp_transport_id) like '%%@lid'
-          or whatsapp_transport_id in (
-            select duplicated.whatsapp_transport_id
-            from (
-              select whatsapp_transport_id
-              from %I.users
-              where deleted_at is null
-                and whatsapp_transport_id is not null
-                and btrim(whatsapp_transport_id) <> ''
-              group by whatsapp_transport_id
-              having count(*) > 1
-            ) duplicated
+    if exists (
+      select 1
+      from information_schema.tables
+      where table_schema = company_record.schema_name
+        and table_name = 'users'
+    ) then
+      execute format($sql$
+        update %I.users
+        set whatsapp_transport_id = null
+        where whatsapp_transport_id is not null
+          and (
+            lower(whatsapp_transport_id) like '%%@lid'
+            or whatsapp_transport_id in (
+              select duplicated.whatsapp_transport_id
+              from (
+                select whatsapp_transport_id
+                from %I.users
+                where deleted_at is null
+                  and whatsapp_transport_id is not null
+                  and btrim(whatsapp_transport_id) <> ''
+                group by whatsapp_transport_id
+                having count(*) > 1
+              ) duplicated
+            )
           )
-        )
-    $sql$, company_record.schema_name, company_record.schema_name);
+      $sql$, company_record.schema_name, company_record.schema_name);
+    end if;
   end loop;
 end
 $$;
